6bc8038b7e5f0c0c73f31803b1fdfdd9375bcf02
dylanstechmann <131562144+dylanstechmann@users.noreply.github.com>
2026-09-16 09:53:52 -0400

    Add Reaper Delay game shell, styles, and engine

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "reaper-delay-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./ai.js",
+  "./content.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+  event.respondWith(
+    caches.match(req).then((hit) => hit || fetch(req).then((res) => {
+      const copy = res.clone();
+      if (res.ok && new URL(req.url).origin === location.origin) {
+        caches.open(CACHE).then((c) => c.put(req, copy));
+      }
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
